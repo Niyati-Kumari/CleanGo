6e5f453bea148f86a40e600b4f38044f54c1b5a8
Niyati Kumari <niyati@Niyatis-MacBook-Air.local>
2026-07-01 16:42:53 +0530

    Updated

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['https://clean-go-peach.vercel.app/'],
+    origin: ['https://clean-go-peach.vercel.app'],
     credentials: true,
   })
 );
